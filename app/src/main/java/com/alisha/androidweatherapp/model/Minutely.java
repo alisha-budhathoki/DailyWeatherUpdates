7be71856149deae1c3071d304e810619eaf2ecd1
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2020-09-16 21:52:22 +0545

    adding model classes for retrofit

--- app/src/main/java/com/alisha/androidweatherapp/model/Minutely.java
+++ app/src/main/java/com/alisha/androidweatherapp/model/Minutely.java
@@ -1,4 +1,35 @@
 package com.alisha.androidweatherapp.model;
 
-public class Minutely {
+public class Minutely
+{
+    private String dt;
+
+    private String precipitation;
+
+    public String getDt ()
+    {
+        return dt;
+    }
+
+    public void setDt (String dt)
+    {
+        this.dt = dt;
+    }
+
+    public String getPrecipitation ()
+    {
+        return precipitation;
+    }
+
+    public void setPrecipitation (String precipitation)
+    {
+        this.precipitation = precipitation;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [dt = "+dt+", precipitation = "+precipitation+"]";
+    }
 }
+
--- app/src/main/java/com/alisha/androidweatherapp/model/WeatherModel.java
+++ app/src/main/java/com/alisha/androidweatherapp/model/WeatherModel.java
@@ -1,4 +1,82 @@
 package com.alisha.androidweatherapp.model;
 
-public class WeatherModel {
+public class WeatherModel
+{
+    private Current current;
+
+    private String timezone;
+
+    private String timezone_offset;
+
+    private String lon;
+
+    private Minutely[] minutely;
+
+    private String lat;
+
+    public Current getCurrent ()
+    {
+        return current;
+    }
+
+    public void setCurrent (Current current)
+    {
+        this.current = current;
+    }
+
+    public String getTimezone ()
+    {
+        return timezone;
+    }
+
+    public void setTimezone (String timezone)
+    {
+        this.timezone = timezone;
+    }
+
+    public String getTimezone_offset ()
+    {
+        return timezone_offset;
+    }
+
+    public void setTimezone_offset (String timezone_offset)
+    {
+        this.timezone_offset = timezone_offset;
+    }
+
+    public String getLon ()
+    {
+        return lon;
+    }
+
+    public void setLon (String lon)
+    {
+        this.lon = lon;
+    }
+
+    public Minutely[] getMinutely ()
+    {
+        return minutely;
+    }
+
+    public void setMinutely (Minutely[] minutely)
+    {
+        this.minutely = minutely;
+    }
+
+    public String getLat ()
+    {
+        return lat;
+    }
+
+    public void setLat (String lat)
+    {
+        this.lat = lat;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [current = "+current+", timezone = "+timezone+", timezone_offset = "+timezone_offset+", lon = "+lon+", minutely = "+minutely+", lat = "+lat+"]";
+    }
 }

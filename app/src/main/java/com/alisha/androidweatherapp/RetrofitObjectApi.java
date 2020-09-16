92627308d9742961d1f05ff8f08abec1eaac3c9e
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2020-09-16 21:46:32 +0545

    first setup commit

---
+++ app/src/main/java/com/alisha/androidweatherapp/RetrofitObjectApi.java
@@ -0,0 +1,10 @@
+package com.alisha.androidweatherapp;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface RetrofitObjectApi {
+    String API_KEY = "8ec853da3fd8310b12325387024a9651";
+//    @GET("data/2.5/onecall?lat=27.6909&lon=85.4118&exclude=hourly,daily&appid="+API_KEY)
+//    Call
+}
